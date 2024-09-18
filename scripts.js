41a19557adeb47eddf56deed269d5c02c1ed717c
Ritika <109076709+ritikasharma1505@users.noreply.github.com>
2024-09-18 20:36:41 +0530

    Updated scripts.js
    API Invoke url updated

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
+var API_ENDPOINT = "https://3tz758f6t9.execute-api.us-east-1.amazonaws.com/prod";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
